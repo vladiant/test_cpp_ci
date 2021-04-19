0a9f69966c390802a73d1a4416665273419e6b3a
Vladislav Antonov <vladislav.antonov@gmail.com>
2021-04-19 07:54:48 +0300

    Update test tag

--- test/test_simple_calc.cpp
+++ test/test_simple_calc.cpp
@@ -3,33 +3,33 @@
 #include <simple_calc.hpp>
 
 namespace vva {
-TEST_CASE("TestAdd_OneToTwo_Three", "[single-file]") {
+TEST_CASE("TestAdd_OneToTwo_Three", "[simple-calc]") {
   REQUIRE(add(1, 2) == 3);
 }
 
-TEST_CASE("TestAdd_UnsignedIntOverflow", "[single-file]") {
+TEST_CASE("TestAdd_UnsignedIntOverflow", "[simple-calc]") {
   constexpr uint8_t a = 255;
   constexpr uint8_t b = 1;
   // Note: 8 bit integer will be printed as char
   REQUIRE(add(a, b) == 0);
 }
 
-TEST_CASE("TestAdd_SignedIntOverflow", "[single-file]") {
+TEST_CASE("TestAdd_SignedIntOverflow", "[simple-calc]") {
   constexpr int8_t a = 127;
   constexpr int8_t b = 1;
   // Note: 8 bit integer will be printed as char
   REQUIRE(int(add(a, b)) == -128);
 }
 
-TEST_CASE("TestSubtract_ThreeByFive_Two", "[single-file]") {
+TEST_CASE("TestSubtract_ThreeByFive_Two", "[simple-calc]") {
   REQUIRE(subtract(5, 3) == 2);
 }
 
-TEST_CASE("TestMultiply_FiveBySix_Thirty", "[single-file]") {
+TEST_CASE("TestMultiply_FiveBySix_Thirty", "[simple-calc]") {
   REQUIRE(multiply(5, 6) == 30);
 }
 
-TEST_CASE("TestDivide_TenByTwo_Five", "[single-file]") {
+TEST_CASE("TestDivide_TenByTwo_Five", "[simple-calc]") {
   REQUIRE(divide(10, 2) == 5);
 }
 
