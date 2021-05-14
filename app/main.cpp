907c3141b643a47ea5912ff3d8ff3985adbfa665
Vladislav Antonov <vladislav.antonov@gmail.com>
2021-05-15 00:11:12 +0300

    Revert "Auto Format"
    This reverts commit 141456e1a304a41a0e8b72fba0617968f65a3e88.

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-#include "basic_warper.hpp"
-#include "checked_warper.hpp"
-#include "clamped_warper.hpp"
 #include "simple_calc.hpp"
+#include "basic_warper.hpp"
+#include "clamped_warper.hpp"
+#include "checked_warper.hpp"
 
 int main() {
   const int a = 4;
@@ -15,34 +15,22 @@ int main() {
   std::cout << "a/b = " << vva::divide(a, b) << '\n';
 
   vva::BasicOperationWarper basic_warper;
-  std::cout << "BasicOperationWarper a+b = " << basic_warper.addition(a, b)
-            << '\n';
-  std::cout << "BasicOperationWarper a-b = " << basic_warper.subtraction(a, b)
-            << '\n';
-  std::cout << "BasicOperationWarper a*b = "
-            << basic_warper.multiplication(a, b) << '\n';
-  std::cout << "BasicOperationWarper a/b = " << basic_warper.division(a, b)
-            << '\n';
+  std::cout << "BasicOperationWarper a+b = " << basic_warper.addition(a, b) << '\n';
+  std::cout << "BasicOperationWarper a-b = " << basic_warper.subtraction(a, b) << '\n';
+  std::cout << "BasicOperationWarper a*b = " << basic_warper.multiplication(a, b) << '\n';
+  std::cout << "BasicOperationWarper a/b = " << basic_warper.division(a, b) << '\n';
 
   vva::ClampedOperationWarper clamped_warper;
-  std::cout << "ClampedOperationWarper a+b = " << clamped_warper.addition(a, b)
-            << '\n';
-  std::cout << "ClampedOperationWarper a-b = "
-            << clamped_warper.subtraction(a, b) << '\n';
-  std::cout << "ClampedOperationWarper a*b = "
-            << clamped_warper.multiplication(a, b) << '\n';
-  std::cout << "ClampedOperationWarper a/b = " << clamped_warper.division(a, b)
-            << '\n';
+  std::cout << "ClampedOperationWarper a+b = " << clamped_warper.addition(a, b) << '\n';
+  std::cout << "ClampedOperationWarper a-b = " << clamped_warper.subtraction(a, b) << '\n';
+  std::cout << "ClampedOperationWarper a*b = " << clamped_warper.multiplication(a, b) << '\n';
+  std::cout << "ClampedOperationWarper a/b = " << clamped_warper.division(a, b) << '\n';
 
   vva::CheckedOperationWarper checked_warper;
-  std::cout << "CheckedOperationWarper a+b = " << checked_warper.addition(a, b)
-            << '\n';
-  std::cout << "CheckedOperationWarper a-b = "
-            << checked_warper.subtraction(a, b) << '\n';
-  std::cout << "CheckedOperationWarper a*b = "
-            << checked_warper.multiplication(a, b) << '\n';
-  std::cout << "CheckedOperationWarper a/b = " << checked_warper.division(a, b)
-            << '\n';
+  std::cout << "CheckedOperationWarper a+b = " << checked_warper.addition(a, b) << '\n';
+  std::cout << "CheckedOperationWarper a-b = " << checked_warper.subtraction(a, b) << '\n';
+  std::cout << "CheckedOperationWarper a*b = " << checked_warper.multiplication(a, b) << '\n';
+  std::cout << "CheckedOperationWarper a/b = " << checked_warper.division(a, b) << '\n';
 
   return 0;
 }
