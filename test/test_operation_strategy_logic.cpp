ec58dd435f8911f4d4a57274d5997b8b11d16422
Vladislav Antonov <vladislav.antonov@gmail.com>
2021-05-22 00:11:59 +0300

    Fix unused field

--- test/test_operation_strategy_logic.cpp
+++ test/test_operation_strategy_logic.cpp
@@ -20,7 +20,6 @@ class MockedOperationStrategyTest {
 TEST_CASE_METHOD(MockedOperationStrategyTest,
                  "MockOperationStrategy_Arg_Result",
                  "[mock-operation-strategy]") {
-  fakeit::Mock<IOperationWarper> mockWarper;
 
   constexpr int16_t first_arg = 6;
   constexpr int16_t second_arg = 2;
