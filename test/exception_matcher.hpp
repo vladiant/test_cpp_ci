8c3be593a0176a00c75fbbdd23d7bea29e43b897
Vladislav Antonov <vladislav.antonov@gmail.com>
2026-06-14 01:19:25 +0300

    Fix ignore list

--- test/exception_matcher.hpp
+++ test/exception_matcher.hpp
@@ -2,15 +2,6 @@
 #include <catch2/catch.hpp>
 #include <string>
 
-// __msan_unpoison_string is used below to suppress false positives when
-// reading the char* returned by std::exception::what(), whose backing memory
-// is allocated by uninstrumented libstdc++ and therefore has no MSan shadow.
-#if defined(__has_feature)
-#if __has_feature(memory_sanitizer)
-#include <sanitizer/msan_interface.h>
-#endif
-#endif
-
 namespace Catch {
 namespace Matchers {
 class ExceptionWatcher : public MatcherBase<std::exception> {
@@ -18,18 +9,8 @@ class ExceptionWatcher : public MatcherBase<std::exception> {
   ExceptionWatcher(std::string const& expected_message)
       : expected_message_(expected_message) {}
 
-  // e.what() returns a const char* backed by uninstrumented libstdc++ memory.
-  // MSan has no shadow bits for it and flags any read (e.g. strlen inside
-  // operator==) as use-of-uninitialized-value.  __msan_unpoison_string marks
-  // the string as initialised before comparing, making the check correct.
   bool match(std::exception const& e) const override {
-    const char* what_msg = e.what();
-#if defined(__has_feature)
-#if __has_feature(memory_sanitizer)
-    __msan_unpoison_string(what_msg);
-#endif
-#endif
-    return what_msg == expected_message_;
+    return e.what() == expected_message_;
   }
 
   std::string describe() const override {
